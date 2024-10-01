d5e141502e40aa36afb7ed0680ec8e8c8e51693f
Andrea Nunoo <123588571+andreanun@users.noreply.github.com>
2024-09-30 20:34:40 -0400

    Week 4 stuff

---
+++ Week4/script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+    //do stuff
+    //1) See the value of the input
+    //look in document for element
+    let input = document.getElementById("name-input")
+    console.log(input.value)
+    //2) Show a message with that name
+}
