6a7cc7a24b5c700fe485e23427ee399e6402543a
Andrea Nunoo <123588571+andreanun@users.noreply.github.com>
2024-10-27 15:06:52 -0400

    Making some components

---
+++ Week6-7/more-react/src/components/Form.jsx
@@ -0,0 +1,17 @@
+function Form() {
+    return (
+      <form>
+          <label form="linkName">Link Name</label>
+          <input type="text" name="linkName"/>
+          <br></br>
+          <label form="linkURL">Link URL</label>
+          <input type="text" name="linkURL"/>
+          <br></br>
+          <br></br>
+  
+          <input></input>
+      </form>
+    )
+}
+
+  export default Form;
---
+++ Week6-7/more-react/src/components/Table.jsx
@@ -0,0 +1,13 @@
+function Table() {
+    return (
+      <table>
+            <tr>
+              <th>Link Name</th>
+              <th>Link URL</th>
+            </tr>
+      </table>
+  
+    )
+}
+
+export default Table;
