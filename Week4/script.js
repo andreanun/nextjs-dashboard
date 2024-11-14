e38731718e39f4f1bf0aaa17ae28f3883aafef2c
Andrea Nunoo <123588571+andreanun@users.noreply.github.com>
2024-11-14 18:38:18 -0500

    cleaning up duplicates

--- Week4/script.js
+++
@@ -1,8 +0,0 @@
-function showMessage() {
-    //do stuff
-    //1) See the value of the input
-    //look in document for element
-    let input = document.getElementById("name-input")
-    console.log(input.value)
-    //2) Show a message with that name
-}
--- Week5/learning-react/src/pages/Welcome.jsx
+++
@@ -1,9 +0,0 @@
-function Welcome() {
-    return (
-        <div>
-            <h1>Welcome all! Let's build a new component</h1>
-        </div>
-    )
-};
-
-export default Welcome;
